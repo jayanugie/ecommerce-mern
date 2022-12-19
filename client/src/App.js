78128a79ef65e66fced7f9c5c0dca2932e99a92e
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2022-12-19 23:30:59 +0700

    turn react into SPA

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main>
         <Container>
-          <HomePage />
+          <Routes>
+            <Route path="/" element={<HomePage />} exact />
+            <Route path="/product/:id" element={<ProductPage />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="primary" variant="dark" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">GieShop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>GieShop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link>Cart</Nav.Link>
-              <Nav.Link>Login</Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>Cart</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>Login</Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
