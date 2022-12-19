f819e0f3df8038474b0f91f127e77dbc62ad7740
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2022-12-19 22:33:47 +0700

    add header and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import { Container } from "react-bootstrap";
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, React</h1>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to GieShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
