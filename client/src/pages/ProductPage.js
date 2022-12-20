4b81266efc1f564fc98cb0b51c65409be1fd04b4
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2022-12-20 20:55:47 +0700

    show product by id

--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -1,11 +1,70 @@
-import React from 'react'
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+// import Rating
+import products from "../products";
 
 const ProductPage = () => {
-  return (
-    <div>
-        <h1>Product Page</h1>
-    </div>
-  )
-}
+  const { id } = useParams();
+  const product = products.find((p) => p._id === id);
 
-export default ProductPage
+  if (!product) return null;
+
+  return (
+    <>
+      <Link className="btn btn-light my-3" to="/">
+        Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid></Image>
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <p>
+                {product.rating} from {product.numReviews}
+              </p>
+            </ListGroup.Item>
+            <ListGroup.Item>Price: {product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>{product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>{product.countInStock}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Add to Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ProductPage;
