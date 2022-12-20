f57014b79fd5ed05aa8988c71512d45ca3d7750e
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2022-12-20 21:34:09 +0700

    setup backend with express

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: "server is running" });
+});
+
+const PORT = process.env.APP_PORT || 5000;
+
+app.listen(PORT, console.log(`server is running on http://localhost:${PORT}`));
